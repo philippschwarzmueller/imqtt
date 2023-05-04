7c0f4d709ada9c8e7ab808ac26173ede17f88369
luntiet <laurinub@yahoo.de>
2023-05-04 21:45:45 +0200

    create log files

--- controller_src/mqtt_sub.c
+++ controller_src/mqtt_sub.c
@@ -6,16 +6,55 @@
 // #include </Users/luntiet/.brew/include/mosquitto.h>
 #include <fcntl.h>
 #include <time.h>
+#include <sys/stat.h>
 
 int	fd;
+
+char	**split(char const *s, char c);
+char	**ft_free(char **str);
+
+void	append_string(char *dest, char *src, int *i)
+{
+	while (*src)
+	{
+		dest[*i] = *src;
+		src++;
+		*i += 1;
+	}
+}
+
+char	*join_three(char *day, char *month, char *time)
+{
+	char	*logname;
+	int		size;
+	int		i;
+
+	i = 0;
+	size = strlen(month) + strlen(day) + strlen(time) + 7;
+	logname = malloc(size);
+	append_string(logname, "log/", &i);
+	append_string(logname, day, &i);
+	logname[i++] = '_';
+	append_string(logname, month, &i);
+	logname[i++] = '_';
+	append_string(logname, time, &i);
+	logname[size] = '\0';
+	return (logname);
+}
 
 char	*get_logname(void)
 {
 	char	*logname;
+	char	**splited;
 	time_t	t;
+	
 
 	time(&t);
 	logname = ctime(&t);
+	splited = split(logname, ' ');
+	logname = join_three(splited[2], splited[1], splited[3]);
+	logname = strcat(logname, ".log");
+	ft_free(splited);
 	return (logname);
 }
 
@@ -46,9 +85,9 @@ int	main(void)
 	struct mosquitto	*mosq;
 	char				*logname;
 
+	mkdir("log", 0755);
 	logname = get_logname();
-	printf("%s", logname);
-	// fd = open(logname, O_CREAT, 0557);
+	fd = open(logname, O_CREAT | O_APPEND | O_TRUNC, 0664);
 	mosquitto_lib_init();
 	id = 12;
 	mosq = mosquitto_new("subscrite-test", true, &id);
@@ -70,4 +109,103 @@ int	main(void)
 	mosquitto_lib_cleanup();
 	close(fd);
 	return (0);
+}
+
+static size_t	ft_word_length(const char *s, size_t i, char c)
+{
+	size_t	count;
+
+	count = 0;
+	while (s[i] != c && s[i])
+	{
+		count++;
+		i++;
+	}
+	return (count);
+}
+
+static char	*ft_fillstr(char const *s, size_t i, char c)
+{
+	char	*stl;
+	size_t	k;
+	size_t	t;
+
+	k = 0;
+	t = ft_word_length(s, i, c);
+	stl = calloc(t + 1, sizeof(char));
+	if (!stl)
+		return (NULL);
+	while (s[i] != c && s[i])
+	{
+		stl[k] = s[i];
+		k++;
+		i++;
+	}
+	return (stl);
+}
+
+static int	ft_count_words(const char *str, char c)
+{
+	int	i;
+	int	trigger;
+
+	i = 0;
+	trigger = 0;
+	if (c == 0)
+		return (1);
+	while (*str)
+	{
+		if (*str != c && trigger == 0)
+		{
+			trigger = 1;
+			i++;
+		}
+		else if (*str == c)
+			trigger = 0;
+		str++;
+	}
+	return (i);
+}
+
+char	**ft_free(char **str)
+{
+	int	i;
+
+	i = 0;
+	while (str[i])
+	{
+		free(str[i]);
+		i++;
+	}
+	free(str);
+	return (NULL);
+}
+
+char	**split(char const *s, char c)
+{
+	size_t	i;
+	size_t	j;
+	char	**str;
+
+	if (!s)
+		return (NULL);
+	str = calloc(ft_count_words(s, c) + 1, sizeof(char *));
+	if (!str)
+		return (NULL);
+	i = 0;
+	j = 0;
+	while (s[i])
+	{
+		while (s[i] == c)
+			i++;
+		if (s[i] != c && s[i])
+		{
+			str[j] = ft_fillstr(s, i, c);
+			if (!str[j])
+				return (ft_free(str));
+			i += strlen(str[j]);
+		}
+		j++;
+	}
+	return (str);
 }
