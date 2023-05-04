38a6388ea2124b21375fa4e3fc8f514bc85efcea
kurz-m <m.kurz92@gmail.com>
2023-05-04 14:02:54 +0200

    Start of the client and the controller

---
+++ client/mqtt_pub.c
@@ -0,0 +1,46 @@
+/* ************************************************************************** */
+/*                                                                            */
+/*                                                        :::      ::::::::   */
+/*   mqtt_pub.c                                         :+:      :+:    :+:   */
+/*                                                    +:+ +:+         +:+     */
+/*   By: makurz <dumba@42.fr>                       +#+  +:+       +#+        */
+/*                                                +#+#+#+#+#+   +#+           */
+/*   Created: 2023/05/03 18:40:06 by makurz            #+#    #+#             */
+/*   Updated: 2023/05/03 19:21:39 by makurz           ###   ########.fr       */
+/*                                                                            */
+/* ************************************************************************** */
+
+#include <stdio.h>
+#include <stdlib.h>
+#include <mosquitto.h>
+#include <unistd.h>
+
+int	main(void)
+{
+	int					rc;
+	struct mosquitto	*mosq;
+
+	mosquitto_lib_init();
+	mosq = mosquitto_new("publisher-test", true, NULL);
+
+	rc = mosquitto_connect(mosq, "localhost", 1883, 60);
+	if (rc != 0)
+	{
+		printf("Client could not connect to broker! Error code: %i\n", rc);
+		mosquitto_destroy(mosq);
+		return (-1);
+	}
+	printf("We are now connected to the broker!\n");
+	int i = 0;
+	while (i < 10)
+	{
+		mosquitto_publish(mosq, NULL, "test/t1", 6, "Hello", 0, false);
+		++i;
+		sleep(3);
+	}
+
+	mosquitto_disconnect(mosq);
+	mosquitto_destroy(mosq);
+	mosquitto_lib_cleanup();
+	return (0);
+}
---
+++ controller/mqtt_sub.c
@@ -0,0 +1,62 @@
+/* ************************************************************************** */
+/*                                                                            */
+/*                                                        :::      ::::::::   */
+/*   mqtt_sub.c                                         :+:      :+:    :+:   */
+/*                                                    +:+ +:+         +:+     */
+/*   By: makurz <dumba@42.fr>                       +#+  +:+       +#+        */
+/*                                                +#+#+#+#+#+   +#+           */
+/*   Created: 2023/05/03 19:22:07 by makurz            #+#    #+#             */
+/*   Updated: 2023/05/03 19:34:51 by makurz           ###   ########.fr       */
+/*                                                                            */
+/* ************************************************************************** */
+
+#include <stdio.h>
+#include <stdlib.h>
+#include <unistd.h>
+#include <mosquitto.h>
+
+void	on_connect(struct mosquitto *mosq, void *obj, int rc)
+{
+	printf("ID: %i\n", * (int *) obj);
+	if (rc != 0)
+	{
+		printf("Error with result code: %i\n", rc);
+		exit(-1);
+	}
+	mosquitto_subscribe(mosq, NULL, "test/t1", 0);
+}
+
+void	on_message(struct mosquitto *mosq, void *obj, const struct mosquitto_message *msg)
+{
+	(void) mosq;
+	(void) obj;
+	printf("New message with topic %s: %s\n", msg->topic, (char *) msg->payload);
+}
+
+int	main(void)
+{
+	int					rc;
+	int					id;
+	struct mosquitto	*mosq;
+
+	mosquitto_lib_init();
+	id = 12;
+	mosq = mosquitto_new("subscrite-test", true, &id);
+	mosquitto_connect_callback_set(mosq, on_connect);
+	mosquitto_message_callback_set(mosq, on_message);
+
+	rc = mosquitto_connect(mosq, "localhost", 1883, 10);
+	if (rc != 0)
+	{
+		printf("Could not connect to Broker with return code %i\n", rc);
+		return (-1);
+	}
+	mosquitto_loop_start(mosq);
+	printf("Press enter to quit...\n");
+	getchar();
+	mosquitto_loop_stop(mosq, true);
+	mosquitto_disconnect(mosq);
+	mosquitto_destroy(mosq);
+	mosquitto_lib_cleanup();
+	return (0);
+}
