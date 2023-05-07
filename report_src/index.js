4fbee88af13b0237ffdd11eb73dd08cc9e455b5a
Philipp Schwarzmüller <philipp.schwarzmueller@gmail.com>
2023-05-07 21:34:55 +0200

    add table view for report

--- report_src/index.js
+++ report_src/index.js
@@ -12,10 +12,10 @@ async function processLineByLine(filepath) {
   });
 
   for await (const line of rl) {
-    let temp = line.split(':');
+    let temp = line.split(":");
     result.push(parseFloat(temp[1]));
   }
-  return (result);
+  return result;
 }
 
 const compareLogs = async (filepath_orig, filepath_received) => {
@@ -23,22 +23,34 @@ const compareLogs = async (filepath_orig, filepath_received) => {
   let received_values = await processLineByLine(filepath_received);
   let result = [];
 
-  for (let i = 0; i < original_values.length; i++)
-  {
-    if (original_values[i] === received_values[i]){
-      console.log(`diff at original: ${original_values[i]} received ${received_values[i]}`);
-      result.push(`diff at original: ${original_values[i]} received ${received_values[i]}`);
-    }
+  for (let i = 0; i < original_values.length; i++) {
+    console.log(
+      `diff at original: ${original_values[i]} received ${received_values[i]}`
+    );
+    result.push({
+      original: original_values[i],
+      received: received_values[i],
+      error: original_values[i] != received_values[i] ? true : false
+  });
   }
-  return (result);
-}
+  return result;
+};
 
 // initialize express app that responds to calls on localhost:3000
 const app = express();
 
-app.get('/', async (req, res) => {
-  let diff = await compareLogs('../sensordata_src/sensor_simulated_data.txt', '../log/5_May_11:00:04.log')
-  res.send(diff);
-})
+app.get("/", async (req, res) => {
+  res.sendFile("./index.html", { root: __dirname });
+});
 
-app.listen(3000, () => console.log("imqtt report server listening on port 3000"));
+app.get("/create_report", async (req, res) => {
+  let data = await compareLogs(
+    "../sensordata_src/sensor_simulated_data.txt",
+    "../log/5_May_11:00:04.log"
+  );
+  res.json(data);
+});
+
+app.listen(3000, () =>
+  console.log("imqtt report server listening on port 3000")
+);
